2e0b19d0cbc3e91aca15e1b2b7961ff3d6a51976
Jing Barry <82812313+jingbarry@users.noreply.github.com>
2023-07-02 17:47:44 +0800

    Update install-3ds-touch-drivers-2022.sh

--- install-3ds-touch-drivers-2022.sh
+++ install-3ds-touch-drivers-2022.sh
@@ -21,9 +21,8 @@ sudo cp TouchDriver2022_04_04/usr/lib/libPhantomIOLib42.so /usr/lib
 echo "--- Create and configure shared directory for configuration files"
 sudo mkdir -p /usr/share/3DSystems/config
 
-echo "--- Create profile.d configurationfile"
-echo "export GTDD_HOME=/usr/share/3DSystems" > 3ds-touch-drivers.sh
-sudo cp 3ds-touch-drivers.sh /etc/profile.d/3ds-touch-drivers.sh
+echo "--- Create GTDD_HOME in /etc/environment"
+sudo sh -c 'echo "GTDD_HOME=/usr/share/3DSystems" >> /etc/environment'
 
 echo "--- Removing temporary files"
 cd $CUR_DIR
